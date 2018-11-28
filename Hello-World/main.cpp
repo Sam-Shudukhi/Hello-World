877ca19dce1ac42dac83ababb09e8e83f29c2bd1
Sam Shudukhi <alshuduki@gmail.com>
2018-11-28 20:02:56 +0300

    Initial Commit

---
+++ Hello-World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello-World
+//
+//  Created by Hussam Alshudukhi on 2018-11-28.
+//  Copyright © 2018 Hussam Alshudukhi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
